c8d7c4fbb44448229610b363580461a3a7711f2e
vmuthabuku <vmuthabuku@gmail.com>
2021-01-12 19:29:43 +0300

    -- add project

--- src/projectss.js
+++ src/projectss.js
@@ -1,4 +1,12 @@
 export const portfolio = [
+{
+    title: "Tinder-Clone",
+    type:"Frontend",
+    description: "A Tinder clone website written in react and uses firebase database and firebase for hosting",
+    sourcecodeurl: "https://github.com/vmuthabuku/tinder-clone",
+    site:"https://tinder-clone-9de69.web.app/",
+
+},
     {
         title: "Movie-Search",
         type:"Frontend",
@@ -25,45 +33,12 @@ export const portfolio = [
        
       },
       {
-        title: "Game-Review",
-        type:"Backend + Frontend",
-        description: "A website written in node js for backend and react js for front-end ",
-        site: "https://github.com/vmuthabuku/Game-Review"
-      },
-      {
         title: "To-do-list",
         type:"Frontend",
         description: "A simple to-do-app written in react js ",
         sourcecodeurl: "https://github.com/vmuthabuku/to-do-list",
         site:"https://todo-appp.netlify.com/"
-      },
-      {
-        title: "Node-js trial",
-        type:"Front-End + Backend",
-        description: "This is a simple trial node js app that uses socket io and mongo db ",
-        sourcecodeurl: "https://github.com/vmuthabuku/nodejs-jquery-simpleapp",
-        site: "https://vmuthabuku.github.io/nodejs-jquery-simpleapp/"
-      },    
-      {
-        title: "Questioner",
-        type:"Front-End",
-        description: "A website written in pure html and css no frame works used",
-        sourcecodeurl: "https://github.com/vmuthabuku/Questioner",
-        site: "https://vmuthabuku.github.io/Questioner/UI/"
-      },
-    
-    {
-        title: "Store-Manager-api",
-        type:"Backend",
-        description: "This is a Store Manager API written in python and tested using travis Ci",
-        site: "https://github.com/vmuthabuku/Store-Manager"
-      },
-      {
-        title: "Questioner-Api",
-        type:"Backend",
-        description: "This is a Questioner API written in python and tested using travis Ci",
-        site: "https://github.com/vmuthabuku/Questioner-api-v1",
-      },
+      },     
       {
           title: "Pixel-Art",
           type:"Front-End",
